2266b58eeb80219fb308df25c26cf770a54501f2
Mari <charmee2928@gmail.com>
2020-05-27 16:38:58 +0900

    Update STEP_W1_HW2.java

--- STEP_W1_HW2.java
+++ STEP_W1_HW2.java
@@ -2,44 +2,44 @@ import java.util.*;
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 /**
- * This program get a random word from "I can haz wordz" as an input 
- * and returns an anagram in a dictionary which has a highest score.
- * @author ashigam
- *
- */
+* This program get a random word from "I can haz wordz" as an input 
+* and returns an anagram in a dictionary which has a highest score.
+* @author ashigam
+*
+*/
 public class STEP_W1_HW2 {
 
     public static void main(String[] args) {            
-    	
-    	// test for HOME WORK 2 	
-    	startGame();
-//    	for(int i=0; i<answers.size(); i++)
-//    		System.out.println(answers.get(i));
-    	
+
+        // test for HOME WORK 2 	
+        startGame();
+    //    	for(int i=0; i<answers.size(); i++)
+    //    		System.out.println(answers.get(i));
+
 
     }
     static void startGame() {
-    	Scanner sc = new Scanner(System.in);
-    	// initialize settings
-    	String[] dict = createDictionary();    	// O(S)
-    	// each word in dictionary now has a char frequency table
-    	HashMap<int[], String> newDict = createNewDict(dict); 	// O(SC)
-    	ArrayList<String> anagrams = new ArrayList<String>();
-    	
-    	int count = 0;
-    	while(count < 10) {
-    		String randomW="";
-    		anagrams.clear();
-    		for(int i =0; i< 16; i++) {    			
-    			randomW += sc.next();        		
-    		}   
-    		System.out.println(randomW);
-    		ArrayList<String> answers = findAnagram2(randomW, newDict, anagrams);
-    		//get the word has a highest score
-        	System.out.println(getHighPWord(answers)); 
-        	count++;
-    	}
-    	sc.close();    	
+        Scanner sc = new Scanner(System.in);
+        // initialize settings
+        String[] dict = createDictionary();    	// O(S)
+        // each word in dictionary now has a char frequency table
+        HashMap<int[], String> newDict = createNewDict(dict); 	// O(SC)
+        ArrayList<String> anagrams = new ArrayList<String>();
+
+        int count = 0;
+        while(count < 10) {
+            String randomW="";
+            anagrams.clear();
+            for(int i =0; i< 16; i++) {    			
+                randomW += sc.next();        		
+            }   
+            System.out.println(randomW);
+            ArrayList<String> answers = findAnagram2(randomW, newDict, anagrams);
+            //get the word has a highest score
+            System.out.println(getHighPWord(answers)); 
+            count++;
+        }
+        sc.close();    	
     }
     /**
      * [HOME WORK 2] Get a word has a highest score in O(WC) time
@@ -48,30 +48,30 @@ public class STEP_W1_HW2 {
      * @return a word has a highest score 
      */
     static String getHighPWord(ArrayList<String> allAnagrams) {
-    	String highPWord = "";
-    	int maxScore = 0;
-    	int[] scoreTable = new int[] {1,1,2,1,1,2,1,2,1,3,3,2,2,1,1,2,3,1,1,1,1,2,2,3,2,3}; 
-    	
-    	// calculate the score for each anagram
-    	for(int i = 0; i < allAnagrams.size(); i++) {	// O(W)
-    		String current = allAnagrams.get(i);
-    		int j = 0;
-    		int score = 0;
-    		while(j < current.length()) {	// O(C)
-    			char c = current.charAt(j);
-    			int index = getCharNumber(c);	// map to scoreTable O(1)
-    			score += scoreTable[index];
-    			j++;
-    		}
-    		// update the current max
-    		if(Math.max(maxScore, score) == score) {
-    			maxScore = score;
-    			highPWord = current;
-    		}   			
-    	}
-    	highPWord.replace("q", "qu");	// back to original
-    	System.out.println(maxScore);
-    	return highPWord;
+        String highPWord = "";
+        int maxScore = 0;
+        int[] scoreTable = new int[] {1,1,2,1,1,2,1,2,1,3,3,2,2,1,1,2,3,1,1,1,1,2,2,3,2,3}; 
+
+        // calculate the score for each anagram
+        for(int i = 0; i < allAnagrams.size(); i++) {	// O(W)
+            String current = allAnagrams.get(i);
+            int j = 0;
+            int score = 0;
+            while(j < current.length()) {	// O(C)
+                char c = current.charAt(j);
+                int index = getCharNumber(c);	// map to scoreTable O(1)
+                score += scoreTable[index];
+                j++;
+            }
+            // update the current max
+            if(Math.max(maxScore, score) == score) {
+                maxScore = score;
+                highPWord = current;
+            }   			
+        }
+        highPWord.replace("q", "qu");	// back to original
+        System.out.println(maxScore);
+        return highPWord;
     }
     /**
      * [HOME WORK 2] Find an anagram without using a generated wordList in O(S+C+SC+SA) = O(SA) time
@@ -83,16 +83,16 @@ public class STEP_W1_HW2 {
      * 		   an error message if it's not found
      */
     static ArrayList<String> findAnagram2(String randomS, HashMap<int[], String> newDict, ArrayList<String> anagrams) {
-    	randomS = randomS.replace("Qu", "q");	// treat as one letter    	
-    	randomS = randomS.toLowerCase();
-    	int[] table = buildCharFreqTable(randomS);	// O(C)     	
-    	
-    	for(int[] i : newDict.keySet()) {	// O(S)
-    		if(isSubSet(i, table)) {		// tables have common items  O(A)
-    			anagrams.add(newDict.get(i));
-    		}
-    	}
-    	return anagrams;
+        randomS = randomS.replace("Qu", "q");	// treat as one letter    	
+        randomS = randomS.toLowerCase();
+        int[] table = buildCharFreqTable(randomS);	// O(C)     	
+
+        for(int[] i : newDict.keySet()) {	// O(S)
+            if(isSubSet(i, table)) {		// tables have common items  O(A)
+                anagrams.add(newDict.get(i));
+            }
+        }
+        return anagrams;
     }
     /**
      * [HOME WORK 2] Check if a dictionary word is a subset of an given anagram in O(A+A) = O(A) time
@@ -103,21 +103,21 @@ public class STEP_W1_HW2 {
      * @return
      */
     static boolean isSubSet(int[] dictWord, int[] anagram) {    	
-    	int validChars = 0;	// the number of alphabets used in the anagram
-    	for(int i : anagram)
-    		if(i != 0)	validChars++;  
+        int validChars = 0;	// the number of alphabets used in the anagram
+        for(int i : anagram)
+            if(i != 0)	validChars++;  
 
-    	int count = 0;
-    	for(int i = 0; i < anagram.length; i++) {	// O(A)
-    		if(anagram[i] == 0 && dictWord[i] != 0)	return false; // cannot make anagram
-    		if(dictWord[i] > anagram[i])			return false;
-    		//if(anagram[i] == 0 && dictWord[i] == 0) continue;	  // both is 0 so it's fine    		
-    		if(anagram[i] != 0 && dictWord[i] != 0 && dictWord[i] <= anagram[i])
-    			count++;    		
-    	}    	    	
-    	if(count <= validChars )
-    		return true;    	 		
-    	return false;
+        int count = 0;
+        for(int i = 0; i < anagram.length; i++) {	// O(A)
+            if(anagram[i] == 0 && dictWord[i] != 0)	return false; // cannot make anagram
+            if(dictWord[i] > anagram[i])			return false;
+            //if(anagram[i] == 0 && dictWord[i] == 0) continue;	  // both is 0 so it's fine    		
+            if(anagram[i] != 0 && dictWord[i] != 0 && dictWord[i] <= anagram[i])
+                count++;    		
+        }    	    	
+        if(count <= validChars )
+            return true;    	 		
+        return false;
     }
     /**
      * [HOME WORK 2] Create a new version of dictionary in O(SC) time
@@ -126,10 +126,10 @@ public class STEP_W1_HW2 {
      * @return a new dictionary in which each word has a char frequency table
      */
     static HashMap<int[], String> createNewDict(String[] dict){
-    	HashMap<int[], String> newDict = new HashMap<int[], String>();
-    	for(String s : dict) 	// O(S)
-    		newDict.put(buildCharFreqTable(s), s);	// O(C)
-    	return newDict;
+        HashMap<int[], String> newDict = new HashMap<int[], String>();
+        for(String s : dict) 	// O(S)
+            newDict.put(buildCharFreqTable(s), s);	// O(C)
+        return newDict;
     }
     /**
      * [HOME WORK 2] Count how many times each character appears in a word in O(C) time
@@ -138,14 +138,14 @@ public class STEP_W1_HW2 {
      * @return a character frequency table
      */
     static int[] buildCharFreqTable(String str) {
-    	int[] table = new int[Character.getNumericValue('z')-
-    	                      Character.getNumericValue('a')+1];
-    	for(char c : str.toCharArray()) {
-    		int x = getCharNumber(c);
-    		if(x != -1)		// character is a letter
-    			table[x]++;   
-    	}
-    	return table;
+        int[] table = new int[Character.getNumericValue('z')-
+                              Character.getNumericValue('a')+1];
+        for(char c : str.toCharArray()) {
+            int x = getCharNumber(c);
+            if(x != -1)		// character is a letter
+                table[x]++;   
+        }
+        return table;
     }
     /**
      * [HOME WORK 2] Map each character to a number in O(1) constant time.
@@ -155,12 +155,12 @@ public class STEP_W1_HW2 {
      * 		   -1 if it's non-letter character
      */
     static int getCharNumber(char c) {
-    	int a = Character.getNumericValue('a');
-    	int z = Character.getNumericValue('z');
-    	int val = Character.getNumericValue(c);
-    	if(a <= val && val <= z)	// char is in a to z
-    		return val - a;
-    	return -1;					// non-letter char
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if(a <= val && val <= z)	// char is in a to z
+            return val - a;
+        return -1;					// non-letter char
     }    
     /**
      * [Helper] Create a dictionary in O(S) time
@@ -168,22 +168,22 @@ public class STEP_W1_HW2 {
      * @return a string array of dictionary
      */
     static String[] createDictionary(){
-    	ArrayList<String> lines = new ArrayList<String>();
-    	try {
-    	      File myObj = new File("../dictionary.txt");
-    	      Scanner myReader = new Scanner(myObj);
-    	      while (myReader.hasNextLine()) {
-    	        String data = myReader.nextLine();
-    	        data.toLowerCase();			// convert to lower case
-    	        data.replace("qu", "q");	// treat as same as a given random word
-    	        lines.add(data);	
-    	      }   	      
-    	      myReader.close();
-    	    } catch (FileNotFoundException e) {
-    	      System.out.println("An error occurred.");
-    	      e.printStackTrace();
-    	    }
-    	String[] dict = lines.toArray(new String[lines.size()]);
-    	return dict;    	
+        ArrayList<String> lines = new ArrayList<String>();
+        try {
+              File myObj = new File("../dictionary.txt");
+              Scanner myReader = new Scanner(myObj);
+              while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                data.toLowerCase();			// convert to lower case
+                data.replace("qu", "q");	// treat as same as a given random word
+                lines.add(data);	
+              }   	      
+              myReader.close();
+            } catch (FileNotFoundException e) {
+              System.out.println("An error occurred.");
+              e.printStackTrace();
+            }
+        String[] dict = lines.toArray(new String[lines.size()]);
+        return dict;    	
     }
 }
