6802471de7f238fb916032b86611be3cdcbaa59d
Mari <charmee2928@gmail.com>
2020-05-30 01:42:13 +0900

    Update STEP_W2_HW4.java

--- STEP_W2_HW4.java
+++ STEP_W2_HW4.java
@@ -2,24 +2,23 @@ import java.util.*;
 /**
 * This program updates the cache every time URL is accessed.
 * It uses HashMap to achieve search, add, remove an element in O(1)
-* and uses customized doubly-linked-list to update the cache in O(1) 
-* except in time to remove tail. Please run cacheTest() to check test cases. 
+* and uses customized doubly-linked-list to update the cache in O(1).
+* Please run cacheTest() to check test cases. 
 * @author ashigam
 */
-    public class STEP_W2_HW4 {
+public class STEP_W2_HW4 {
     // declare instance variables
-    private Node head;
+    private Node head = null;
+    private Node tail = null;
     private int cacheSize;
-    private HashMap<String, Node> map;
+    private HashMap<String, Node> map = new HashMap<String, Node>();;
 
     /**
-     * Constructor: initialize cache size, HashMap, and head Node.
+     * Constructor: initialize cache size, HashMap, and head Node 
      * @param N the number of cache size 
      */
     public STEP_W2_HW4(int N){
-        cacheSize = N;
-        map = new HashMap<String, Node>();
-        head = null;
+        this.cacheSize = N;
     }
     // Node for DLList
     class Node{
@@ -38,28 +37,68 @@ import java.util.*;
      */
     void accessPage(String url, String contents) {
 
-        // contents are not in the map: add data to HashMap 
-        // and use the URL data as a node for DLList 
-        if(!this.map.containsKey(contents)) {	
-
-            // map is full: remove tail and push to head
-            if(map.size() == cacheSize)	{	
-                removeTail();	// remove from DLList and from map
-                Node u = new Node(url, contents);
-                map.put(contents, u);
-                push(u);
-            }
-            // map has enough space: push to head
-            else if(map.size() < cacheSize) {
-                Node u = new Node(url, contents);
-                map.put(contents, u);
-                push(u);				
-            }				
-        }
         // contents are in the map: move it up to head
-        else {			
+        if(map.containsKey(contents)) {
             moveUp(map.get(contents));
+            return;
         }
+        // map is full: remove tail and push to head
+        if(map.size() == cacheSize) {
+            removeTail();
+        }
+        // add data to HashMap and use the URL data as a node for DLList 
+        Node u = new Node(url, contents);
+        map.put(contents, u);
+        push(u);				
+    }
+    /**
+     * Helper: Move a node up to the head by updating DLList pointers
+     * @param u the node to be moved up
+     */
+    void moveUp(Node u) {
+        if(u.prev == null)	// u is head
+            return;	
+        if(u.next == null) {// u is tail
+            u.prev.next = null;		
+            tail = u.prev;
+        }
+        else {			  	// u is in middle		
+            u.prev.next = u.next;
+            u.next.prev = u.prev;
+        }		
+        // bring u to head
+        u.prev = null;
+        head.prev = u;
+        u.next = head;
+        head = u;
+    }
+    /**
+     * Helper: Remove a tail from DLList and from HashMap 
+     */
+    void removeTail() {
+        String contents = tail.contents;
+        if(tail.prev == null ) {	// tail only
+            tail = null;
+            head = null;
+        }
+        else {	// tail has a prev node
+            tail.prev.next = null;
+            tail = tail.prev;			
+        }
+        map.remove(contents);		
+    }
+    /**
+     * Helper: Push a node to the head of the DLList
+     * @param newN the node to be pushed to the head
+     */
+    void push(Node newN) {
+        newN.next = head;
+        newN.prev = null;
+        if(head != null) 
+            head.prev = newN;	
+        else
+            tail = newN;
+        head = newN;
     }
     /**
      * Return the URLs stored in the cache. The URLs are ordered
@@ -116,51 +155,7 @@ import java.util.*;
         cache.accessPage("f.com", "FFF");
         System.out.println(Arrays.equals(cache.getPages(), new String[] {"f.com","e.com","a.com","c.com"}));			
     }
-    /**
-     * Helper: Move a node up to the head by updating DLList pointers.
-     * @param u the node to be moved up
-     */
-    void moveUp(Node u) {
-        if(u.prev == null)	// u is head
-            return;	
-        if(u.next == null) {// u is tail
-            u.prev.next = null;				
-        }
-        else {			  	// u is in middle		
-            u.prev.next = u.next;
-            u.next.prev = u.prev;
-        }		
-        // bring u to head
-        u.prev = null;
-        head.prev = u;
-        u.next = head;
-        head = u;
-    }
-    /**
-     * Helper: Remove a tail from DLList and from HashMap in O(N).
-     */
-    void removeTail() {
-        Node last = head;
-        while(last.next != null) 
-            last = last.next;
-        // now the node reached to tail
-        String contents = last.contents;
-        last.prev.next = null;		
-        map.remove(contents);		
-    }
-    /**
-     * Helper: Push a node to the head of the DLList.
-     * @param newN the node to be pushed to the head
-     */
-    void push(Node newN) {
-        newN.next = head;
-        newN.prev = null;
-        if(head != null) 
-            head.prev = newN;		
-        head = newN;
-    }
-
     public static void main(String args[]) {		
-        cacheTest();	  
+        cacheTest();	  			
     }
 }
